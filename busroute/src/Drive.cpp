529a69e75f9696f6fd100d44ae39b38668a99234
Emil T. Pedersen <etpe19@student.aau.dk>
2019-11-14 15:44:01 +0100

    Kør x antal meter

--- busroute/src/Drive.cpp
+++ busroute/src/Drive.cpp
@@ -2,36 +2,41 @@
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
 
-int dist(){
+float PosX = 0;
+float PosY = 0;
+float PrePosX;
+float PrePosY;
+float dist;
+float driven = 0;
 
+void chatterCallback(const nav_msgs::Odometry::ConstPtr& msg){
+    PosX = msg->pose.pose.position.x;
+    PosY = msg->pose.pose.position.y;
+    dist = sqrt(((PosX*PosX)+(PosY*PosY)));
+    std::cout << dist << std::endl;
+    if(dist < 1){
+        
+    }
 }
 
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "Drive");
 
+    std::cout << "Hvor langt skal den køre?" << std::endl;
+    std::cin >> dist;
+
     ros::NodeHandle n;
     ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/teleop", 1);
 
-        //    ros::Subscriber odom_sub = n.subscribe("odom", 100, dist);
+    ros::Subscriber odom_sub = n.subscribe("odom", 1, chatterCallback);
+
+    std::cout << PrePosX << ", " << PrePosY << std::endl;
 
     geometry_msgs::Twist cmd_vel_message;
     cmd_vel_message.angular.z = 0.0;
     cmd_vel_message.linear.x = 0.50;
 
     ros::Rate loop_rate(10);
-    for (int i = 0; i < 20; i++)
-{
-
-        cmd_vel_message.angular.z = cmd_vel_message.angular.z * 1.00;
-        cmd_vel_message.linear.x = cmd_vel_message.linear.x * 1.00;
-        cmd_vel_pub.publish(cmd_vel_message);
-        loop_rate.sleep();
-}
-
-        cmd_vel_message.angular.z = cmd_vel_message.angular.z * 0.00;
-        cmd_vel_message.linear.x = cmd_vel_message.linear.x * 0.00;
-        cmd_vel_pub.publish(cmd_vel_message);
-        loop_rate.sleep();
-    ros::spinOnce();
+    ros::spin();
 }
