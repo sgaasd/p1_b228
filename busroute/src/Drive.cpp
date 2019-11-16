8982fe23ba69749347f19570a102d4917d1c95f9
Jonathan Falk <jfalk19@student.aau.dk>
2019-11-16 10:49:55 +0100

    Oprettet Drive2.cpp

--- busroute/src/Drive.cpp
+++ busroute/src/Drive.cpp
@@ -51,35 +51,7 @@ int main(int argc, char *argv[])
             cmd_vel_pub.publish(cmd_vel_message);
         }
 
-<<<<<<< HEAD
-    geometry_msgs::Twist cmd_vel_message;
-    cmd_vel_message.angular.z = 0.0;
-    cmd_vel_message.linear.x = 0.25;
 
-//1 meter --> x = 0.25 --> for_loop = 40
-//1 meter --> x = 0.50 --> for_loop = 20
-//1 meter --> x = 1 --> for_loop = loop_rate
-
-    ros::Rate loop_rate(10);
-<<<<<<< HEAD
-    for (int i = 0; i < 40; i++)
-{
-
-        cmd_vel_message.angular.z = cmd_vel_message.angular.z * 1.00;
-        cmd_vel_message.linear.x = cmd_vel_message.linear.x * 1.00;
-        cmd_vel_pub.publish(cmd_vel_message);
-        loop_rate.sleep();
-}
-
-        cmd_vel_message.angular.z = cmd_vel_message.angular.z * 0.00;
-        cmd_vel_message.linear.x = cmd_vel_message.linear.x * 0.00;
-        cmd_vel_pub.publish(cmd_vel_message);
-        loop_rate.sleep();
-    ros::spinOnce();
-=======
-    ros::spin();
->>>>>>> 6d913cffa8e11ab219bfa56f3492e78f2cd486f6
-=======
         std::cout << driven << " out of " << dist << " driven." << std::endl;
 
         //std::cout << PrePosX << ", " << PrePosY << std::endl;
@@ -87,5 +59,4 @@ int main(int argc, char *argv[])
     
     return 0;
 
->>>>>>> a5fedaa876cdabb6f708866e5089eacec9fa7792
 }
