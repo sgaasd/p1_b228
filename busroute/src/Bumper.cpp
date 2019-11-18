0f8c2bf8f18358e0feaba1c6a61f51b2fd107b71
Emil T. Pedersen <etpe19@student.aau.dk>
2019-11-18 10:46:42 +0100

    bumper_cmake_lort

---
+++ busroute/src/Bumper.cpp
@@ -0,0 +1,26 @@
+#include <ros/ros.h>
+#include <kobuki_msgs/BumperEvent.h>
+
+
+// kobuki_msgs.msg import BumperEvent
+
+
+/*void BumperCallback(const kobuki_msgs/BumperEvent::ConstPtr& msg)
+{
+    if (uint8 LEFT = 0 & unit8 PRESSED = 1)
+    {
+        
+    }
+    
+//uint8 LEFT   = 0
+//uint8 CENTER = 1
+//uint8 RIGHT  = 2
+}
+*/
+int main(int argc, char const *argv[])
+{
+    ros::init(argc, argv, "Bumper");
+    ros::NodeHandle n;
+   // ros::Subscriber bumper_sub = n.subscribe("mobile_base/events/bumper", BumperEvent, processBump, 1, BumperCallback);
+    return 0;
+}
