58aea222cbbf52284715228586e0c4f0a2da6c94
SanHanona <shanon19@student.aau.dk>
2019-11-20 13:50:51 +0100

    bumper milestone_2

--- busroute/src/DriveStop.cpp
+++ busroute/src/DriveStop.cpp
@@ -6,26 +6,28 @@ using namespace std;
 
 ros::Publisher cmd_vel_pub;
 
-class move{
-    public:
-        void BumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg){
-            geometry_msgs::Twist cmd_vel_message;
-            bool hit = msg->state;
-            int bump = msg->bumper;
-            ros::Rate loop_rate(1);
-            if (hit == 1) {
-                cmd_vel_message.angular.z = 0.00;
-                cmd_vel_message.linear.x = -0.10;
-                cmd_vel_pub.publish(cmd_vel_message);
-                cout << cmd_vel_message << endl;
-                loop_rate.sleep();
-                cmd_vel_message.angular.z = 0.78;
-                cmd_vel_message.linear.x = 0.00;
-                cmd_vel_pub.publish(cmd_vel_message);
-                cout << cmd_vel_message << endl;
-                }
-        }   
-};
+void BumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg){
+    geometry_msgs::Twist cmd_vel_message;
+    bool hit = msg->state;
+    int bump = msg->bumper;
+    ros::Rate loop_rate(21);
+    if (hit == 1) {
+        for(int i=0; i<=42; i++){ 
+            cmd_vel_message.angular.z = 0.00;
+            cmd_vel_message.linear.x = -0.10;
+            cmd_vel_pub.publish(cmd_vel_message);
+            cout << cmd_vel_message << endl;
+            loop_rate.sleep();
+        }
+        for(int i=0; i<=42; i++){
+            cmd_vel_message.angular.z = 0.50;
+            cmd_vel_message.linear.x = 0.00;
+            cmd_vel_pub.publish(cmd_vel_message);
+            cout << cmd_vel_message << endl;
+            loop_rate.sleep();
+        }
+    }
+}   
 
 int main(int argc, char *argv[])
 {
@@ -33,10 +35,8 @@ int main(int argc, char *argv[])
 
     ros::NodeHandle n;
 
-    move something;
-
     cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/safety_controller", 1);
-    ros::Subscriber Bumper_sub = n.subscribe("mobile_base/events/bumper", 1, &move::BumperCallback, &something);
+    ros::Subscriber Bumper_sub = n.subscribe("mobile_base/events/bumper", 1, BumperCallback);
 
     ros::spin();
     return 0;
