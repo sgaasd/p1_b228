6182133a0bfd0c6d1f1072c1cd27fa0d0ad6253f
SanHanona <shanon19@student.aau.dk>
2019-11-21 10:38:04 +0100

    Bumper_Done

--- busroute/src/DriveStop.cpp
+++ busroute/src/DriveStop.cpp
@@ -1,61 +1,85 @@
+//Necessary libraries for the program are included 
 #include <ros/ros.h>
 #include <kobuki_msgs/BumperEvent.h>
 #include <geometry_msgs/Twist.h>
 #include <math.h>
 using namespace std;
 
+//Publisher varible is initialised 
 ros::Publisher cmd_vel_pub;
 
-/*geometry_msgs::Twist vel_msg(int t1, int t2, int bumper, const kobuki_msgs::BumperEvent::ConstPtr& msg2){
-    ros::Rate loop_rate(21);
-    for(int i=0; i<=t1*21; i++){ 
-            cmd_vel_message.angular.z = 0.00;
-            cmd_vel_message.linear.x = -0.10;
-            cmd_vel_pub.publish(cmd_vel_message);
-            cout << cmd_vel_message << endl;
-            loop_rate.sleep();
-    }
-    for(int i=0; i<=t1*21; i++){
-        cmd_vel_message.angular.z = 0.50;
-        cmd_vel_message.linear.x = 0.00;
-        cmd_vel_pub.publish(cmd_vel_message);
-        cout << cmd_vel_message << endl;
-        loop_rate.sleep();
-    }
-}*/
+//A function for defining cmd_vel_message is created  
+geometry_msgs::Twist BumpMsg(float x, float z){
+    geometry_msgs::Twist cmd_vel_message;
+    cmd_vel_message.angular.z = z;
+    cmd_vel_message.linear.x = x;
+    cout << cmd_vel_message << endl;
+    return cmd_vel_message;
+}
 
+//The function BumperCallBack tells if the bumper is pressed and which side 
 void BumperCallback(const kobuki_msgs::BumperEvent::ConstPtr& msg){
     geometry_msgs::Twist cmd_vel_message;
     bool hit = msg->state;
     int bump = msg->bumper;
     ros::Rate loop_rate(21);
+    //The function should only should only start if the bumper is pressed 
     if (hit == 1) {
-        for(int i=0; i<=42; i++){ 
-            cmd_vel_message.angular.z = 0.00;
-            cmd_vel_message.linear.x = -0.10;
-            cmd_vel_pub.publish(cmd_vel_message);
-            cout << cmd_vel_message << endl;
-            loop_rate.sleep();
-        }
-        for(int i=0; i<=42; i++){
-            cmd_vel_message.angular.z = 0.50;
-            cmd_vel_message.linear.x = 0.00;
-            cmd_vel_pub.publish(cmd_vel_message);
-            cout << cmd_vel_message << endl;
-            loop_rate.sleep();
+        //The robot should make different turns depending on which bumper is pressed 
+        switch (bump){
+            //If the bumper on the left side is pressed the robot will turn right
+            case 0:
+                for(int i=0; i<=42; i++){ 
+                    cmd_vel_pub.publish(BumpMsg(-0.2, 0.0));
+                    loop_rate.sleep();
+                }
+                for(int i=0; i<=42; i++){
+                    cmd_vel_pub.publish(BumpMsg(0.0, -0.3));
+                    cout << cmd_vel_message << endl;
+                    loop_rate.sleep();
+                }
+            break;
+            //If the bumper on the middle is pressed the bumper will turn left 
+            case 1:
+                for(int i=0; i<=42; i++){ 
+                    cmd_vel_pub.publish(BumpMsg(-0.2, 0.0));
+                    loop_rate.sleep();
+                }
+                for(int i=0; i<=42; i++){
+                    cmd_vel_pub.publish(BumpMsg(0.0, 0.3));
+                    loop_rate.sleep();
+                }
+            break;
+            //If the bumper on the right side is pressed the bumper will turn left 
+            case 2:
+                for(int i=0; i<=42; i++){ 
+                    cmd_vel_pub.publish(BumpMsg(-0.2, 0.0));
+                    loop_rate.sleep();
+                }
+                for(int i=0; i<=42; i++){
+                    cmd_vel_pub.publish(BumpMsg(0.0, 0.3));
+                    loop_rate.sleep();
+                }
+            break;
         }
     }
 }   
 
 int main(int argc, char *argv[])
 {
+    //DriveStop_node is initialised
     ros::init(argc, argv, "DriveStop");
 
+    //NodeHandle is defined 
     ros::NodeHandle n;
 
+    //cmd_vel_pub is defined 
     cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/safety_controller", 1);
+    //Subcribing to "mobile_base/events/bumper". 
+    //Evertime an advertisment is made on the topic "mobile_base/events/bumper" run "BumperCallBack"
     ros::Subscriber Bumper_sub = n.subscribe("mobile_base/events/bumper", 1, BumperCallback);
 
+    //The program has run continuously 
     ros::spin();
     return 0;
 }
