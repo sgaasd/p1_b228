7cfeb392b7472ed099dee27d85806727f87c48df
SanHanona <shanon19@student.aau.dk>
2019-11-21 11:29:53 +0100

    Wheel_drop videre

--- busroute/src/WheelDrop.cpp
+++ busroute/src/WheelDrop.cpp
@@ -1,8 +1,15 @@
 #include <ros/ros.h>
-#include <
+#include <kobuki_msgs/WheelDropEvent.h
 using namespace std;
+
+
 
 int main(int argc, char *argv[]){
 
+    ros::init(argc, argv, "WheelDrop");
+    ros::NodeHandle n;
+    ros::Subscriber WheelDrop_sub = n.subscribe("/mobile_base/events/wheel_drop", 1, WheelDrop_callBack);");
+
+    ros::spin();
     return 0;
 }
