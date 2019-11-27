aea2e47d931683b5e9837b5148617d9ab999d159
sgaasd <sgaasd19@student.aau.dk>
2019-11-27 12:50:25 +0100

    Rette MoveGoal med nye x og y værdier

--- busroute/src/MoveGoal.cpp
+++ busroute/src/MoveGoal.cpp
@@ -1,55 +1,40 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
-//#include "sound_play/sound_play.h"
-
-//std::string path_to_sounds;
 
 /** function declarations **/
 bool moveToGoal(double xGoal, double yGoal);
-//char choose();
+char choose();
 
 /** declare the coordinates of interest **/
-double xCafe = 15.50;
-double yCafe = 10.20;
-double xOffice1 = 27.70 ;
-double yOffice1 = 12.50;
-double xOffice2 = 30.44 ;
-double yOffice2 = 12.50;
-double xOffice3 = 35.20 ;
-double yOffice3 = 13.50;
+double xB228 = 1.181052;
+double yB228 = 0.033761;
+double xPrinterRum = 2.200309;
+double yPrinterRum = 6.166436;
+
 
 bool goalReached = false;
 
- int main(int argc, char** argv){
+int main(int argc, char** argv){
    ros::init(argc, argv, "MoveGoal_node");
    ros::NodeHandle n;
-  // sound_play::SoundClient sc;
    ros::spinOnce();
-  // path_to_sounds = "/home/ros/catkin_ws/src/gaitech_edu/src/sounds/";
+  
 
    char choice = 'q';
    do{
-     // choice = choose();
+    choice = choose();
       if (choice == '0'){
-         goalReached = moveToGoal(xCafe, yCafe);
+         goalReached = moveToGoal(xB228, yB228);
       }else if (choice == '1'){
-         goalReached = moveToGoal(xOffice1, yOffice1);
-      }else if (choice == '2'){
-         goalReached = moveToGoal(xOffice2, yOffice2);
-      }else if (choice == '3'){
-         goalReached = moveToGoal(xOffice3, yOffice3);
+         goalReached = moveToGoal(xPrinterRum, yPrinterRum);
       }
       if (choice!='q'){
          if (goalReached){
             ROS_INFO("Congratulations!");
             ros::spinOnce();
-          //  sc.playWave(path_to_sounds+"ship_bell.wav");
-            ros::spinOnce();
-
          }else{
             ROS_INFO("Hard Luck!");
-            //sc.playWave(path_to_sounds+"short_buzzer.wav");
          }
       }
    }while(choice !='q');
@@ -95,5 +80,21 @@ bool moveToGoal(double xGoal, double yGoal){
       ROS_INFO("The robot failed to reach the destination");
       return false;
    }
+
+}
+
+char choose(){
+	char choice='q';
+	std::cout<<"|-------------------------------|"<<std::endl;
+	std::cout<<"|PRESSE A KEY:"<<std::endl;
+	std::cout<<"|'0': B228 "<<std::endl;
+	std::cout<<"|'1': Printer rum "<<std::endl;
+	std::cout<<"|'q': Quit "<<std::endl;
+	std::cout<<"|-------------------------------|"<<std::endl;
+	std::cout<<"|WHERE TO GO?";
+	std::cin>>choice;
+
+	return choice;
+
 
 }
