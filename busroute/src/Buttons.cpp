dc1eee1f622c407939466ae417c559f77f696460
Jonathan Falk <jfalk19@student.aau.dk>
2019-12-02 13:18:45 +0100

    MoveGoalButtons

--- busroute/src/Buttons.cpp
+++ busroute/src/Buttons.cpp
@@ -1,26 +1,43 @@
 #include <ros/ros.h>
 #include <kobuki_msgs/ButtonEvent.h>
 #include <iostream>
+#include <geometry_msgs/Twist.h>
 using namespace std;
 
-void ButtonCallback(const kobuki_msgs::ButtonEvent::ConstPtr& msg){ 
-    bool B0Pressed = msg->state;
-    bool B1Pressed = msg->state;
-    bool B2Pressed = msg->state;
-    int B0 = msg->button;
-    int B1 = msg->button;
-    int B2 = msg->button;
-    
-    if (B0Pressed == true && B0 == 0){
-        std::cout << "B0 = " << B0Pressed << std::endl;
+ros::Publisher cmd_vel_pub;
 
+void ButtonCallback(const kobuki_msgs::ButtonEvent::ConstPtr& msg){ 
+    bool Pressed = msg->state;
+    int Button = msg->button;
+    
+    if (Pressed == true && Button == 0){
+        std::cout << "B0 = " << Pressed << std::endl;
+        std::cin >> Button;
+        if (Button == 0){
+            geometry_msgs::Twist cmd_vel_message;
+            cmd_vel_message.angular.z = 0.5;
+            cmd_vel_message.linear.x = 0.5;
+    
+        }
     }
-    else if (B1Pressed == true && B1 == 1){
-        std::cout << "B1 = " << B1Pressed << std::endl;
+    else if (Pressed == true && Button == 1){
+        std::cout << "B1 = " << Pressed << std::endl;
+        std::cin >> Button;
+        if (Button == 1){
+            geometry_msgs::Twist cmd_vel_message;
+            cmd_vel_message.angular.z = 0.0;
+            cmd_vel_message.linear.x = -0.5;    
+        }
     }
-    else if (B2Pressed == true && B2 == 2){
-        std::cout << "B2 = " << B2Pressed << std::endl;
-    }
+    else if (Pressed == true && Button == 2){
+        std::cout << "B2 = " << Pressed << std::endl;
+        std::cin >> Button;
+        if (Button == 2){
+            geometry_msgs::Twist cmd_vel_message;
+            cmd_vel_message.angular.z = 0.0;
+            cmd_vel_message.linear.x = 0.5;    
+        }
+    }    
     else {
         std::cout << "Du har ikke trykket på en knap" << std::endl;
     }
@@ -33,6 +50,8 @@ int main(int argc, char *argv[])
     ros::NodeHandle n;
     ros::Subscriber Button_sub = n.subscribe("mobile_base/events/button", 10, ButtonCallback);
 
+    ros::NodeHandle nh;
+    cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
 
     ros::spin();
     return 0;
