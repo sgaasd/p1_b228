5c40a1f777290887e9eeaf37b3fab0576d1a634e
Emil T. Pedersen <etpe19@student.aau.dk>
2019-12-03 11:03:38 +0100

    light

--- busroute/src/Lights.cpp
+++ busroute/src/Lights.cpp
@@ -1,31 +1,113 @@
 #include <ros/ros.h>
-#include <geometry_msgs/Twist.h>
-#include <nav_msgs/Odometry.h>
 #include <kobuki_msgs/Led.h>
+#include <string>
+#include <actionlib_msgs/GoalStatusArray.h>
 
-void light(const nav_msgs::Odometry::ConstPtr& msg);
+void light(int a, int b);
+void showStatus(const actionlib_msgs::GoalStatusArray::ConstPtr& msg);
+
 
 ros::Publisher led1_pub;
 ros::Publisher led2_pub;
 
+
 int main(int argc, char *argv[])
 {
-    ros::init(argc, argv, "light_function");
-
+    ros::init(argc, argv, "Drive_function");
     ros::NodeHandle n;
 
     led1_pub = n.advertise<kobuki_msgs::Led>("/mobile_base/commands/led1", 1);
     led2_pub = n.advertise<kobuki_msgs::Led>("/mobile_base/commands/led2", 1);
-    
-    ros::Subscriber led_sub = n.subscribe("led", 1, light);
-    
-    ros::spin();
 
-    return 0;
+    ros::Subscriber odom_sub = n.subscribe("/move_base/status", 1, showStatus);
+
 }
 
-void light(const nav_msgs::Odometry::ConstPtr& msg){
+void showStatus(const actionlib_msgs::GoalStatusArray::ConstPtr& msg){
+    int a = msg->status_list.status;
+    switch (a)
+    {
+    case 1:
+        light(1, g);
+        break;
+    
+    case 2:
+        light(1, g);
+        break;
 
+    case 3:
+        light(1, g);
+        break;
+
+    case 4:
+        light(1, g);
+        break;
+
+    case 5:
+        light(1, g);
+        break;
+
+    case 6:
+        light(1, g);
+        break;
+
+    case 7:
+        light(1, g);
+        break;
+    
+    case 8:
+        light(1, g);
+        break;
+    
+    case 9:
+        light(1, g);
+        break;
+
+    default:
+        std::cout << a << " er ikke en valid status!"<< std::endl;
+        break;
+    
+    }
+
+}
+
+void light(int a, char b){
     kobuki_msgs::Led led_message;
-    led1_pub.publish(led_message);
+    int c;
+    switch (b){
+    case 's':
+        c = 0;
+        break;
+    
+    case 'g':
+        c = 1;
+        break;
+    
+    case 'o':
+        c = 2;
+        break;
+    
+    case 'r':
+        c = 3;
+        break;
+        
+    default:
+        std::cout << b << " er ikke en mulig farve" << std::endl;
+        break;
+    }
+
+    led_message.value = c;
+    
+    switch(a){
+        case 1:
+            led1_pub.publish(led_message);
+            break;
+        case 2:
+            led2_pub.publish(led_message);
+            break;
+
+        default:
+            std::cout << "led " << a << "eksisterer ikke!" std::endl;
+            break;    
+    }
 }
