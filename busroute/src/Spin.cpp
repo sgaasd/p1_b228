2c99e5819335949414d6938c73ecf0960dbd133d
sgaasd <sgaasd19@student.aau.dk>
2019-12-03 15:59:57 +0100

    spin rettet til med quaternioner

--- busroute/src/Spin.cpp
+++ busroute/src/Spin.cpp
@@ -1,26 +1,49 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
+//#include <duration.h>
+
 using namespace std;
 
 //Publisher varible is initialised 
 ros::Publisher cmd_vel_pub;
 
+//void SpinCallBack(const nav_msgs::Odometry::ConstPtr& msg);
+
+
+
 //A function for defining cmd_vel_message is created  
-geometry_msgs::Twist SafetyMsg(float x, float z){
+geometry_msgs::Twist SafetyMsg(float z){
     geometry_msgs::Twist cmd_vel_message;
     cmd_vel_message.angular.z = z;
-    cmd_vel_message.linear.x = x;
     cout << cmd_vel_message << endl;
     return cmd_vel_message;
 }
 
 //The function with for the Wheel Drop. With pointers to Wheel and State 
-    void SpinCallBack(){
+    void SpinCallBack(const nav_msgs::Odometry::ConstPtr& msg){
+        ros::Duration(10.0).sleep();
+        tf::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
+        tf::Matrix3x3 m(q);
+        double roll, pitch, yaw;
+        m.getRPY(roll, pitch, yaw);
+
+        ros::Rate loop_rate(1);
+        cout << yaw << endl;
+
+        if (yaw < 180){
+            cmd_vel_pub.publish(SafetyMsg(1.0));
+             cout << yaw << endl;
+        }
+        loop_rate.sleep();
+
+            /*    ros::Duration four_seconds(4.0);
         ros::Rate loop_rate(21);
         for(int i=0; i<=84; i++){ 
-            cmd_vel_pub.publish(SafetyMsg(0.0, (6.2831853072 / 4))); //Turns 360 degrees
+            cmd_vel_pub.publish(SafetyMsg((6.2831853072 / 4))); //Turns 360 degrees
             loop_rate.sleep();
-            }
+            }*/
         cout << "I am ready" << endl;
 
         exit(1);
@@ -32,11 +55,9 @@ int main(int argc, char *argv[]){
     ros::NodeHandle n;
 
     cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
+    ros::Subscriber odom_sub = n.subscribe("odom", 1, SpinCallBack);
 
-    SpinCallBack(); 
+
 
     return 0; 
 }
-
-
-
