9895a8ab33cf732fb3aef2f34aca91f6eef90d9f
sgaasd <sgaasd19@student.aau.dk>
2019-12-04 12:35:02 +0100

    færdig move og spin

--- busroute/src/Spin.cpp
+++ busroute/src/Spin.cpp
@@ -1,17 +1,9 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include <nav_msgs/Odometry.h>
-#include <tf/transform_datatypes.h>
-//#include <duration.h>
-
 using namespace std;
 
 //Publisher varible is initialised 
 ros::Publisher cmd_vel_pub;
-
-//void SpinCallBack(const nav_msgs::Odometry::ConstPtr& msg);
-
-
 
 //A function for defining cmd_vel_message is created  
 geometry_msgs::Twist SafetyMsg(float z){
@@ -22,31 +14,14 @@ geometry_msgs::Twist SafetyMsg(float z){
 }
 
 //The function with for the Wheel Drop. With pointers to Wheel and State 
-    void SpinCallBack(const nav_msgs::Odometry::ConstPtr& msg){
-        //ros::Duration(10.0).sleep();
-        tf::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
-        tf::Matrix3x3 m(q);
-        double roll, pitch, yaw;
-        m.getRPY(roll, pitch, yaw);
-        ros::spin();
-        
-        cout << yaw << endl;
-
-        if (yaw < 180){
-            cmd_vel_pub.publish(SafetyMsg(1.0));
-             cout << yaw << endl;
-        }
-        
-
-            /*    ros::Duration four_seconds(4.0);
+    void SpinCallBack(){
+        ros::Duration(10.0).sleep();
         ros::Rate loop_rate(21);
-        for(int i=0; i<=100; i++){ 
-            cmd_vel_pub.publish(SafetyMsg((6.2831853072 / 4))); //Turns 360 degrees
+        for(int i=0; i<=90; i++){ 
+            cmd_vel_pub.publish(SafetyMsg(0.6)); 
             loop_rate.sleep();
-            }*/
+            }
         cout << "I am ready" << endl;
-
-        exit(1);
     }   
 
 int main(int argc, char *argv[]){
@@ -55,9 +30,7 @@ int main(int argc, char *argv[]){
     ros::NodeHandle n;
 
     cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel_mux/input/teleop", 1);
-    ros::Subscriber odom_sub = n.subscribe("odom", 1, SpinCallBack);
-
-
-    ros::spin();
+    
+    SpinCallBack();
     return 0; 
 }
