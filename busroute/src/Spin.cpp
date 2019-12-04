d77fad93a86bf3ad05a2083bb29e50314836831d
sgaasd <sgaasd19@student.aau.dk>
2019-12-04 10:45:17 +0100

    ny mocetogoal

--- busroute/src/Spin.cpp
+++ busroute/src/Spin.cpp
@@ -23,20 +23,20 @@ geometry_msgs::Twist SafetyMsg(float z){
 
 //The function with for the Wheel Drop. With pointers to Wheel and State 
     void SpinCallBack(const nav_msgs::Odometry::ConstPtr& msg){
-        ros::Duration(10.0).sleep();
+        //ros::Duration(10.0).sleep();
         tf::Quaternion q(msg->pose.pose.orientation.x, msg->pose.pose.orientation.y, msg->pose.pose.orientation.z, msg->pose.pose.orientation.w);
         tf::Matrix3x3 m(q);
         double roll, pitch, yaw;
         m.getRPY(roll, pitch, yaw);
-
-        ros::Rate loop_rate(1);
+        ros::spin();
+        
         cout << yaw << endl;
 
         if (yaw < 180){
             cmd_vel_pub.publish(SafetyMsg(1.0));
              cout << yaw << endl;
         }
-        loop_rate.sleep();
+        
 
             /*    ros::Duration four_seconds(4.0);
         ros::Rate loop_rate(21);
